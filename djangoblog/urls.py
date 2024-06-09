d76c2a563b5606f86d24bf7ed5b41da6e6f5f08f
Meezan Shaikh <meezan@tradexa.co.in>
2024-06-09 10:21:06 +0530

    extending template flow implemented

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about-us',views.about),
-    path('',views.homepage),
-    path('articles/',include('articles.urls'))
+    path('articles/',include('articles.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
